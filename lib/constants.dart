6f6019101bc43f726c14699adb93049c206f2d23
Adharsh Reghu <adharshreghu2002@gmail.com>
2023-03-29 20:18:55 +0530

    constant page updated

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,13 @@
+//page which contains all the constants which will be used for the development of application
 import 'package:flutter/material.dart';
 
-//page which contains all the constants which will be used for the development of application
-const String kAppfont =
-    "Imprima"; //font to be used in all places in the app except on the login page and on generated ticket cards
-const TextStyle kHeadingStyle = TextStyle(
-    color: Colors.black,
-    fontFamily: kAppfont,
-    fontSize: 25); //Text style to be used on the title of every appbar
+//font to be used in all places in the app except on the login page and on generated ticket cards
+const String kAppfont = "Imprima";
+
+//Text style to be used on the title of every appbar
+const TextStyle kHeadingStyle =
+    TextStyle(color: Colors.black, fontFamily: kAppfont, fontSize: 25);
+
+//bolded normal text
+const TextStyle kNormalText =
+    TextStyle(fontFamily: kAppfont, fontSize: 23, fontWeight: FontWeight.bold);
